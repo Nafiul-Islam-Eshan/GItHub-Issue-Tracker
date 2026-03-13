001649f6737ee958dbae5164e46a156c744c6323
Nafiul-Islam-Eshan <nafiulislameshan307@gmail.com>
2026-03-14 03:49:39 +0600

    modal is  implimented

--- script/issues.js
+++ script/issues.js
@@ -5,10 +5,18 @@ const closeContainer = document.getElementById("closeIssueCards");
 const issueCount = document.getElementById("issueCount");
 const issuesSearch = document.getElementById("issuesSearch")
 
+// Reuseable functions
 const createElement = (arr) => { 
-    const htmlElements = arr.map( el => ` <div class="badge border-gray-300 bg-base-300 text-xs rounded-2xl">${el}</div>`); 
+    const htmlElements = arr.map( el => ` <div class="badge border-gray-300 bg-base-300 text-sm rounded-2xl">${el}</div>`); 
     return htmlElements.join(" "); 
 }
+const formatName = (name) => {
+    const wordsArr = name.split("_")
+    const capitalizedWordsArr = wordsArr.map(word => word[0].toUpperCase() + word.slice(1));
+    return capitalizedWordsArr.join(" ");
+};
+
+
 
 
 
@@ -20,6 +28,79 @@ const loadIssues = () => {
         .then(issues => displayIssues(issues.data));
 }
 
+const loadIssueDetail = async (id) => { 
+  const url = `https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`;
+  const res = await fetch(url); 
+  const detail = await res.json(); 
+  displayIssueDetail(detail.data); 
+}
+
+const displayIssueDetail = (detail) => {
+    const issueDetails = document.getElementById("issueDetails")
+
+    //     {
+    // "id": 33,
+    // "title": "Add bulk operations support",
+    // "description": "Allow users to perform bulk actions like delete, update status on multiple items at once.",
+    // "status": "open",
+    // "labels": [
+    // "enhancement"
+    // ],
+    // "priority": "low",
+    // "author": "bulk_barry",
+    // "assignee": "",
+    // "createdAt": "2024-02-02T10:00:00Z",
+    // "updatedAt": "2024-02-02T10:00:00Z"
+    // }
+
+
+    issueDetails.innerHTML = `
+        <div class="space-y-6">
+            <!-- Upper -->
+            <div class="space-y-1">
+                <h2 class="font-bold text-xl">${detail.title}</h2>
+                <div class="flex gap-2 items-center">
+                    <div class="badge text-white rounded-2xl ${
+                        detail.status==="open"? "bg-[#00A96E]" : "bg-[#A855F7]"
+                    }">${detail.status==="open"? "Opened" : "Closed"}</div>
+                    <div class="rounded-full p-[2px] w-1 bg-gray-400"></div>
+                    <p class="text-gray-500 text-sm">Opened by ${formatName(detail.author)}</p>
+                    <div class="rounded-full p-[2px] w-1 bg-gray-400"></div>
+                    <p class="text-gray-500 text-sm">${
+                            new Date(detail.createdAt).toLocaleDateString('en-GB')
+                        }</p>
+                </div>
+            </div>
+            <!-- Middle 1 -->
+            <div> ${createElement(detail.labels)} </div>
+            <!-- Middle 2 -->
+            <p class="text-gray-500"> ${detail.description} </p>
+                            <!-- Lower -->
+            <div class="bg-base-200 p-4 flex justify-between gap-2 items-center">
+                <!-- left -->
+                <div class="w-1/2">
+                    <p class="text-gray-500 text-sm">Assignee:</p>
+                    <h4 class="text-sm font-semibold">${formatName(detail.author)}</h4>
+                </div>
+                <!-- Right -->
+                <div class="w-1/2">
+                    <p class="text-gray-500 text-sm">Priority:</p>
+                    <div class="badge border-gray-300 bg-base-300 rounded-2xl">${detail.priority}</div>
+                </div>
+            </div>
+        </div>
+
+        <div class="modal-action">
+            <form method="dialog">
+                <button class="btn btn-primary">Close</button>
+            </form>
+        </div>
+    `;
+
+    document.getElementById("my_modal_5").showModal()
+}
+
+
 const displayIssues = (issues) => {
     allContainer.innerHTML = "";
     openContainer.innerHTML = "";
@@ -28,7 +109,7 @@ const displayIssues = (issues) => {
     issues.forEach(issue => {
         const newDiv = document.createElement("div")
         newDiv.innerHTML = `
-                <div class="p-4 shadow-xl border-t-5 rounded-lg space-y-3 h-full ${
+                <div onclick="loadIssueDetail(${issue.id})" class="p-4 shadow-xl border-t-5 rounded-lg space-y-3 h-full ${
                     issue.status === "open" ? "border-t-[#00A96E]" : "border-t-[#A855F7]"
                 }">
 
@@ -60,11 +141,10 @@ const displayIssues = (issues) => {
         // append clones so each container has its own node
         allContainer.append(newDiv.cloneNode(true));
         if (issue.status === "open") {
-            openContainer.append(newDiv.cloneNode(true));
+            openContainer.append(newDiv);
         } else {
-            closeContainer.append(newDiv.cloneNode(true));
+            closeContainer.append(newDiv);
         }
-        document.getElementById("my_modal_5").showModal()
     });
 
     issuesCount();
@@ -73,13 +153,7 @@ const displayIssues = (issues) => {
 loadIssues();
 
 
-const loadIssueDetail = async (id) => { // id = unique id of word details
-  const url = `https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`;
-  const res = await fetch(url); // fetching and getting respond
-  const detail = await res.json();  // converting the respond to json
-  console.log(detail.data); 
-}
-loadIssueDetail(1)
+
 
 
 
